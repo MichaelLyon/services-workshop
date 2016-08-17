7e53d288464f899de90479a391a456c32d4959bd
Michael Tobin <mikel.tobin@gmail.com>
2016-08-17 10:23:22 -0600

    Routes hooked up to the angular workshop app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('servicesWorkshop', []);
+var app = angular.module('servicesWorkshop', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/messages", {
+        templateUrl : "/templates/messages.html",
+        controller: 'MessagesController'
+    })
+    .when("/postmessages", {
+        templateUrl : "/templates/new_message.html",
+        controller:'PostMessageController'
+    })
+    .otherwise("/", {
+        templateUrl : "index.html"
+    });
+});
